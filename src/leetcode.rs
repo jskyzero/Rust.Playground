0c27644cba07a9d38592e4aa853b3339e06de0fe
jskzyero <jskyzero@outlook.com>
2020-11-09 12:53:44 +0800

    update leetcode matrix

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -32,3 +32,35 @@ pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
 
     return result;
 }
+
+struct SubrectangleQueries {
+    rectangle: Vec<Vec<i32>>,
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl SubrectangleQueries {
+
+    fn new(rectangle: Vec<Vec<i32>>) -> Self {
+        let rectangle = SubrectangleQueries {
+            rectangle: rectangle.clone()
+        };
+        return rectangle;
+    }
+    
+    fn update_subrectangle(&mut self, row1: i32, col1: i32, row2: i32, col2: i32, new_value: i32) {
+        for i in row1..row2+1 {
+            for j in col1..col2+1 {
+                self.rectangle[i as usize][j as usize] = new_value;
+                println!("{}", self.rectangle[i as usize][j as usize]);
+            }
+        }
+    }
+    
+    fn get_value(&self, row: i32, col: i32) -> i32 {
+        return self.rectangle[row as usize][col as usize];
+    }
+}
